05c250a87dc43be7eaa237ba19f75fe368dbdaaa
SAURETMathieu <mathieu.sauret@oclock.school>
2024-01-22 17:48:56 +0100

    add sqitch conf with env variables

--- app/middlewares/preventInactivity.js
+++ app/middlewares/preventInactivity.js
@@ -8,9 +8,6 @@ function requestServer() {
           if (!response.ok) {
               console.error(`Échec de la consultation à ${new Date().toLocaleTimeString()} - Code de statut : ${response.status}`);
           }
-          if(response.ok){
-            console.log("ok");
-          }
       })
       .catch(error => {
           console.error(`Erreur lors de la consultation à ${new Date().toLocaleTimeString()}: ${error.message}`);
