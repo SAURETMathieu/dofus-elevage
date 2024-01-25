0b89d47e7cbd1ae95a254adb67be558d6fe42c33
SAURETMathieu <mathieu.sauret@oclock.school>
2024-01-25 09:56:48 +0100

    add new bg and add missing directory serversImage

--- public/js/buttons/checkboxPrivate.js
+++ public/js/buttons/checkboxPrivate.js
@@ -3,11 +3,11 @@ export default function checkboxInit() {
   checkbox.addEventListener('change', (event) => {
     const charactersToHide = document.querySelectorAll('.closed');
     charactersToHide.forEach((characterToHide) => {
-        if (event.target.checked) {
-          characterToHide.style.display = '';
-        } else {
-          characterToHide.style.display = 'none';
-        }
+      if (event.target.checked) {
+        characterToHide.style.display = '';
+      } else {
+        characterToHide.style.display = 'none';
+      }
     });
   });
 }
